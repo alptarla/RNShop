ae1dec1b9d961edec4a16ab60e64f473c78f0464
alptarla <alptarla@gmail.com>
2022-03-30 11:09:34 +0300

    Initial project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Text>RNShop</Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,14 @@
+# RNSHop
+
+## Overview
+
+RNShop is e-commerce app with React Native and fake store api.
+
+Users should be able to:
+
+- Login to app
+- See all products on the home screen
+- See product detail
+- Add product to shopping cart or remove it
+- See all products at the shopping cart
+- Remove all products from the shopping cart
