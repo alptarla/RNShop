0aa29e2dcae4d78c46834721ba20436e85a34343
alptarla <alptarla@gmail.com>
2022-03-30 14:34:27 +0300

    Add navigation hooks

---
+++ src/hooks/useTypedNavigation.ts
@@ -0,0 +1,17 @@
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import {
+  BottomTabsParamList,
+  HomeStackParamList,
+  RootStackParamList,
+} from '../types'
+
+export const useRootNavigation = () =>
+  useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+
+export const useMainTabsNavigation = () =>
+  useNavigation<BottomTabNavigationProp<BottomTabsParamList>>()
+
+export const useHomeNavigation = () =>
+  useNavigation<NativeStackNavigationProp<HomeStackParamList>>()
