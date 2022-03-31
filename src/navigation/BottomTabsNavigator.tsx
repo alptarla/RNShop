892a2398061d7d2995bb21c011ca7d487391f6bc
alptarla <alptarla@gmail.com>
2022-03-31 16:12:18 +0300

    Refactor bottom tab navigator

--- src/navigation/BottomTabsNavigator.tsx
+++ src/navigation/BottomTabsNavigator.tsx
@@ -1,60 +1,46 @@
 import Icon from '@expo/vector-icons/MaterialCommunityIcons'
-import {
-  BottomTabNavigationOptions,
-  createBottomTabNavigator,
-} from '@react-navigation/bottom-tabs'
-import { RouteProp } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
 import CartScreen from '../screens/CartScreen'
 import Colors from '../theme/Colors'
 import { BottomTabsParamList } from '../types'
 import HomeTabNavigator from './HomeTabNavigator'
 
-const BottomTabs = createBottomTabNavigator<BottomTabsParamList>()
-
-type ScreenOptionsParams = {
-  route: RouteProp<BottomTabsParamList, keyof BottomTabsParamList>
-  navigation: any
+const renderTabBarIcon = (name: string) => {
+  return ({ color, size }: { color: string; size: number }) => (
+    <Icon
+      name={name as any}
+      color={color}
+      size={size}
+    />
+  )
 }
 
-const screenOptions = ({
-  route,
-}: ScreenOptionsParams): BottomTabNavigationOptions => ({
-  tabBarShowLabel: false,
-  tabBarActiveTintColor: Colors.primaryDark,
-  tabBarInactiveTintColor: Colors.primary,
-  tabBarIcon: ({ color, size }) => {
-    return route.name === 'HomeTab' ? (
-      <Icon
-        name="shopping"
-        color={color}
-        size={size}
-      />
-    ) : route.name === 'CartScreen' ? (
-      <Icon
-        name="cart"
-        color={color}
-        size={size}
-      />
-    ) : null
-  },
-})
+const BottomTabs = createBottomTabNavigator<BottomTabsParamList>()
 
 const BottomTabsNavigator: React.FC = () => (
   <BottomTabs.Navigator
     initialRouteName="HomeTab"
-    screenOptions={screenOptions}
+    screenOptions={({ route }) => ({
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: Colors.primaryDark,
+      tabBarInactiveTintColor: Colors.primary,
+    })}
   >
     <BottomTabs.Screen
       name="HomeTab"
       component={HomeTabNavigator}
       options={{
         headerShown: false,
+        tabBarIcon: renderTabBarIcon('shopping'),
       }}
     />
     <BottomTabs.Screen
       name="CartScreen"
       component={CartScreen}
+      options={{
+        tabBarIcon: renderTabBarIcon('cart'),
+      }}
     />
   </BottomTabs.Navigator>
 )
